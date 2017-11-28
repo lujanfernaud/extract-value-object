e2ca4a502c5d9313b069e484943aa317d224a326
lujanfernaud <lujanfernaud@gmail.com>
2017-11-28 21:23:39 +0000

    Add Grade value object

---
+++ lib/grade.rb
@@ -0,0 +1,28 @@
+class Grade
+  attr_reader :score, :letter
+
+  def self.letter_for(score)
+    new(score).letter
+  end
+
+  def initialize(score)
+    @score  = score
+    @letter = letter_for_score
+  end
+
+  def score=(score)
+    Grade.new(score)
+  end
+
+  private
+
+  def letter_for_score
+    case score
+    when  1..59 then 'F'
+    when 60..70 then 'D'
+    when 70..80 then 'C'
+    when 80..90 then 'B'
+    else             'A'
+    end
+  end
+end
---
+++ spec/grade_spec.rb
@@ -0,0 +1,66 @@
+require 'spec_helper'
+
+describe Grade do
+  describe '.letter_for' do
+    it 'returns letter for the score given' do
+      score = 99
+      expect(Grade.letter_for score).to eq('A')
+    end
+  end
+
+  describe '#letter' do
+    it 'returns A for a score 90 or greater' do
+      letter = 'A'
+      score  = 99
+
+      expect(described_class.new(score).letter).to eq(letter)
+    end
+
+    it 'returns B for a score between 80 and 90' do
+      letter = 'B'
+      score  = 85
+
+      expect(described_class.new(score).letter).to eq(letter)
+    end
+
+    it 'returns C for a score between 70 and 80' do
+      letter = 'C'
+      score  = 75
+
+      expect(described_class.new(score).letter).to eq(letter)
+    end
+
+    it 'returns D for a score between 60 and 70' do
+      letter = 'D'
+      score  = 65
+
+      expect(described_class.new(score).letter).to eq(letter)
+    end
+
+    it 'returns F for a score less than 60' do
+      letter = 'F'
+      score  = 50
+
+      expect(described_class.new(score).letter).to eq(letter)
+    end
+  end
+
+  describe '#score' do
+    it 'returns grade score' do
+      score  = 50
+      letter = 'F'
+
+      expect(described_class.new(score).letter).to eq(letter)
+    end
+
+    it 'creates a new Grade object when trying to change the score' do
+      initial_grade_score = 50
+      new_grade_score = 99
+      grade = described_class.new(initial_grade_score)
+
+      other_grade = (grade.score = new_grade_score)
+
+      expect(other_grade).not_to eq(grade)
+    end
+  end
+end
